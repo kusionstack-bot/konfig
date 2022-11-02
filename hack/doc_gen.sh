9d2a51c9aa78e01e2054963caa462b5af1a034fc
xiarui.xr <xiarui1994@gmail.com>
2022-11-02 16:01:49 +0800

    add doc_gen script

---
+++ hack/doc_gen.sh
@@ -0,0 +1,25 @@
+rm -rf docs
+
+OUTPUT_DIR="docs/"
+SOURCE_CODE_URL="https://github.com/KusionStack/konfig/tree/main"
+FILES="$PWD/base/pkg"
+
+SCRIPT="kcl-doc generate $FILES -R -o $OUTPUT_DIR --repo-url $SOURCE_CODE_URL"
+
+echo $SCRIPT
+
+$($SCRIPT)
+
+# remove specific directorys which contain backend/render/mixins/utils logic schemas. Only frontend/backend model docs are reserved.
+find ./docs  -type dir -name 'backend' -exec rm -rf {} +
+find ./docs  -type dir -name 'mixins' -exec rm -rf {} +
+find ./docs  -type dir -name 'render' -exec rm -rf {} +
+find ./docs  -type dir -name 'utils' -exec rm -rf {} +
+
+# remove unnecessary nested dirs
+mv docs/base/pkg/* docs/
+rm -rf docs/base
+
+# finanlly, generate the index
+cd docs
+md-file-tree > index.md
